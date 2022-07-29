ebf27738d167fecd112fba319797fccf402ba458
AzureAD\RituranjanGupta <rituranjang@audaxlabs.com>
2022-07-29 09:34:44 +0530

    Error resovled
    Error resovled

--- HighwayMonitoring/HighwayMonitoringCosmosDB/Models/TrafficAnalysis.cs
+++ HighwayMonitoring/HighwayMonitoringCosmosDB/Models/TrafficAnalysis.cs
@@ -4,7 +4,30 @@ using System;
 namespace HighwayMonitoringCosmosDB.Models
 {
     public class TrafficAnalysis
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty("tAcamera_Id")]
+        public string TAcamera_Id { get; set; }
 
+        [JsonProperty("Taccident")]
+        public int Taccident { get; set; }
+
+        [JsonProperty("noaccident")]
+        public int noaccident { get; set; }
+
+        [JsonProperty("tAccidentStatus")]
+        public long tAccidentStatus { get; set; }
+
+
+
+        [JsonProperty("tAframe_timestamp")]
+        public int TAframe_timestamp { get; set; }
+
+        [JsonProperty("tAvideo_Id")]
+        public int tAvideo_Id { get; set; }
+
+    }
 
     public class VehicleAccidentLive
     {
